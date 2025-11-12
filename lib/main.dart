200f387dc5e0e155612e1b8928c660133d9e31c8
Harsh Pant <pantharsh11hp@gmail.com>
2025-11-12 15:46:42 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.system,
 
-      home: HomePage(),
-      initialRoute: '/',
+      // home: HomePage(),
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
